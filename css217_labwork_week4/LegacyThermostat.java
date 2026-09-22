d0a5c317b1ce47c0089201dfe7d826a3b980d12f
Sultan <sultan.ermyrzauly@gmail.com>
2026-09-22 17:09:03 +0500

    Add css217_labwork_week4

---
+++ css217_labwork_week4/LegacyThermostat.java
@@ -0,0 +1,12 @@
+public class LegacyThermostat {
+    // Valid dial states: "IDLE", "LOW", "MEDIUM", "MAX"
+    private String dialPosition = "IDLE";
+
+    public void rotateDial(String position) {
+        this.dialPosition = position;
+    }
+
+    public String checkDial() {
+        return this.dialPosition;
+    }
+}
